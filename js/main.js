c9bb50a04e4b090104c4887a7b7b816459a51ddb
msimbao <32774483+msimbao@users.noreply.github.com>
2019-11-10 16:28:41 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 
+
+// pagepiling
+
 $(document).ready(function() {
 	$('#pagepiling').pagepiling({
 	    menu: null,
         direction: 'vertical',
         verticalCentered: true,
+        sectionsColor: ['#000000', '#000000', '#7BAABE', 'whitesmoke', '#000'],
         anchors: [],
-        scrollingSpeed: 400,
+        scrollingSpeed: 700,
         easing: 'swing',
         loopBottom: false,
         loopTop: false,
         css3: true,
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['section1', 'section2', 'section3', 'section4']
+        },
        	normalScrollElements: null,
         normalScrollElementTouchThreshold: 5,
         touchSensitivity: 5,
@@ -21,8 +31,47 @@ $(document).ready(function() {
 		onLeave: function(index, nextIndex, direction){},
 		afterLoad: function(anchorLink, index){},
 		afterRender: function(){},
-    
-    //User Interface
-    sectionsColor: ['#5214ff', '#ffffff','#ECCED6','#F2C6BD'],
 	});
 });
+
+// Blob Colors and Control
+
+const colors = ['#fff','#fff','#fff'];
+const numLines = 3;
+var currCount = numLines;
+const texts = document.querySelectorAll("#textClip text");
+const blobs = document.querySelectorAll("#background path");
+
+function colorBlobs() {
+    blobs.forEach(blob => {
+        blob.style.fill = colors[Math.floor(Math.random() * colors.length)];
+    });
+}
+
+function nextIteration() {
+    // Change the color of all the blobs
+    colorBlobs();
+
+    // Hide the old set of lines
+    let startVal = currCount - numLines;
+    if(startVal < 0) {
+        startVal = texts.length - numLines;
+    }
+    for(let i = startVal; i < startVal + numLines; i++) {
+        texts[i].style.display = "none";
+    }
+    // Show new set of lines
+    for(let j = currCount; j < currCount + numLines; j++) {
+        texts[j].style.display = "inline";
+    }
+    currCount += numLines;
+    if(currCount >= texts.length) {
+        currCount = 0;
+    }
+}
+
+// Since all of our blobs are using the same animation, we only
+// need to listen to one of them
+blobs[0].addEventListener("animationiteration", nextIteration);
+
+colorBlobs();
