4c8cf84b62557da5d51b68b31be1a271d1714e9a
msimbao <32774483+msimbao@users.noreply.github.com>
2019-11-14 09:08:37 -0600

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
 	    menu: null,
         direction: 'vertical',
         verticalCentered: true,
-        sectionsColor: ['#002C53', '#FE3808', '#002C53', '#FE3808', '#002C53'],
+        sectionsColor: ['#002C53', '#002C53', '#002C53', '#FE3808', '#002C53'],
         anchors: [],
         scrollingSpeed: 700,
         easing: 'swing',
@@ -264,3 +264,47 @@ window.location.reload();
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+
+
+  // Blob Colors and Control
+
+const colors = ['#FE3808','#002C53','#00AEE0'];
+const numLines = 3;
+var currCount = numLines;
+const texts = document.querySelectorAll("#textClip text");
+const blobs = document.querySelectorAll("#background path");
+
+function colorBlobs() {
+    blobs.forEach(blob => {
+        blob.style.fill = colors[Math.floor(Math.random() * colors.length)];
+    });
+}
+
+function nextIteration() {
+    // Change the color of all the blobs
+    colorBlobs();
+
+    // Hide the old set of lines
+    let startVal = currCount - numLines;
+    if(startVal < 0) {
+        startVal = texts.length - numLines;
+    }
+    for(let i = startVal; i < startVal + numLines; i++) {
+        texts[i].style.display = "none";
+    }
+    // Show new set of lines
+    for(let j = currCount; j < currCount + numLines; j++) {
+        texts[j].style.display = "inline";
+    }
+    currCount += numLines;
+    if(currCount >= texts.length) {
+        currCount = 0;
+    }
+}
+
+// Since all of our blobs are using the same animation, we only
+// need to listen to one of them
+blobs[0].addEventListener("animationiteration", nextIteration);
+
+colorBlobs();
