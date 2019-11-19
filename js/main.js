52a39ead5f011aecc53775597d5d0e2337ba5800
msimbao <32774483+msimbao@users.noreply.github.com>
2019-11-19 17:56:40 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -251,10 +251,11 @@ colorBlobs();
     );
 
     $("#modals").append(
-        '<div class="modal" id="'+ short +'">' +
+        '<div class="modal" id="'+ short +'" >' +
+        ' <div class="x_button" data-dismiss="modal" aria-label="Close">&times;</div>' +
     '<div class="row about-extra pop-row test">' +
       '<div class="col-lg-6 modal-words">' +
-        '<h4>'+
+        '<h4  >'+
           item +
         '</h4>' +
         '<p> Location: ' + 
@@ -271,6 +272,8 @@ colorBlobs();
 });
 
 
+$('.close').modal('toggle');
+
 // ========================================================
 // NavBar
 // ========================================================
@@ -282,3 +285,144 @@ function openNav() {
     document.getElementById("myNav").style.width = "0%";
   }
 
+// ========================================================
+// Contact Bar
+// ========================================================
+
+function openContact() {
+  document.getElementById("contactNav").style.width = "50%";
+}
+
+function closeContact() {
+  document.getElementById("contactNav").style.width = "0%";
+}
+
+
+// ========================================================  
+// Write Reuqests To Database 
+// ========================================================
+var first_name = document.getElementById("first_name");
+var email_address = document.getElementById("email_address");
+var comments = document.getElementById("comments");
+var srcData;
+
+var submitBtn = document.getElementById("submit");
+// prints "started" in the browser's dev tools console to help me know that the first part has gone through
+console.log("started request submission");
+
+function contactSubmit() {
+  var name = first_name.value;
+  var email = email_address.value;
+  var comment = comments.value;
+
+  var ref = firebase.app().database().ref();
+      var requestRef = ref.child('requests').push();
+
+      window.alert("Thank you for sending your request! we will get back to you soon!");
+
+      requestRef.child("name").set(name);
+      requestRef.child("email").set(email);
+      requestRef.child("comment").set(comment);
+      // firebaseRef.push().set(messageText);
+}
+
+// ========================================================  
+// Google Script for Request Data via email
+// ========================================================
+
+(function() {
+  // get all data in form and return object
+  function getFormData(form) {
+    var elements = form.elements;
+    var honeypot;
+    var fields = Object.keys(elements).filter(function(k) {
+      if (elements[k].name === "honeypot") {
+        honeypot = elements[k].value;
+        return false;
+      }
+      return true;
+    }).map(function(k) {
+      if(elements[k].name !== undefined) {
+        return elements[k].name;
+      // special case for Edge's html collection
+      }else if(elements[k].length > 0){
+        return elements[k].item(0).name;
+      }
+    }).filter(function(item, pos, self) {
+      return self.indexOf(item) == pos && item;
+    });
+    var formData = {};
+    fields.forEach(function(name){
+      var element = elements[name];
+      
+      // singular form elements just have one value
+      formData[name] = element.value;
+      // when our element has multiple items, get their values
+      if (element.length) {
+        var data = [];
+        for (var i = 0; i < element.length; i++) {
+          var item = element.item(i);
+          if (item.checked || item.selected) {
+            data.push(item.value);
+          }
+        }
+        formData[name] = data.join(', ');
+      }
+    });
+    // add form-specific values into the data
+    formData.formDataNameOrder = JSON.stringify(fields);
+    formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+    formData.formGoogleSend
+      = form.dataset.email || ""; // no email by default
+    return {data: formData, honeypot: honeypot};
+  }
+  function handleFormSubmit(event) {  // handles form submit without any jquery
+    event.preventDefault();           // we are submitting via xhr below
+    var form = event.target;
+    var formData = getFormData(form);
+    var data = formData.data;
+    // If a honeypot field is filled, assume it was done so by a spam bot.
+    if (formData.honeypot) {
+      return false;
+    }
+    disableAllButtons(form);
+    var url = form.action;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          form.reset();
+          var formElements = form.querySelector(".form-elements")
+          if (formElements) {
+            formElements.style.display = "none"; // hide form
+          }
+          var thankYouMessage = form.querySelector(".thankyou_message");
+          if (thankYouMessage) {
+            thankYouMessage.style.display = "block";
+          }
+        }
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+    }).join('&');
+    xhr.send(encoded);
+  }
+  
+  function loaded() {
+    // bind to the submit event of our form
+    var forms = document.querySelectorAll("form.gform");
+    for (var i = 0; i < forms.length; i++) {
+      forms[i].addEventListener("submit", handleFormSubmit, false);
+    }
+  };
+  document.addEventListener("DOMContentLoaded", loaded, false);
+  function disableAllButtons(form) {
+    var buttons = form.querySelectorAll("button");
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+  }
+})();
