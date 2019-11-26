d71defd9009d535d7bec9609b91c2fb23c146ec4
msimbao <32774483+msimbao@users.noreply.github.com>
2019-11-26 11:44:56 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,73 +6,112 @@
 
 //=======================================================
 
+// Author's Note: There are several commented out alert notes. These are for testing the Javascript to let the page tell me whether certain functions have been carried out and what their outputs. 
+// If the output of a function is what is intuitively expected, e.g for a function to increase database search count entries by 1, then that function has passed the test. Else, it has failed
+
+
 // ========================================================
 // Filter Search
 // ========================================================
 
 function searchFunction() {
-
-      // Declare General Variables
-        var input, filter, ul, li, a, i, txtValue;
+  // Declare General Variables
+  var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
   li = ul.getElementsByTagName("li");
 
-// Keyword Related Variables
 
-          var searchterm = document.getElementById("myInput");
-        var searchterm = searchterm.value;
-        alert(searchterm)
-        searchRef = database.ref("search");
-
-// Time Related Variables
-
-        timeRef = database.ref("usageTimes")
-        var date = new Date();
-       var hours = date.getHours();
-
-     // Record Time of Search
-       
-        timeRef.push(hours)
-
-   // Collect Search Keyword Data
-
-           searchRef.child(searchterm).once('value', function(snapshot) {
-          if (snapshot.exists()) {
-           alert('exists');
-            var currentValue = snapshot.child('count').val(); 
-            alert('currentValue:' + currentValue)
-            var newValue = currentValue + 1;
-            alert('NewValue:' + newValue)
-            searchRef.child(searchterm).child('count').set(newValue);
-            // searchRef.child(searchterm).set(newValue);
-          } else {
-            alert('absent');
-            var newValue =  1;
-            searchRef.child(searchterm).child('name').set(searchterm);
-            searchRef.child(searchterm).child('count').set(newValue);
-          }
+  // Handle Enter Key Press
+  var input = document.getElementById("myInput");
+  input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("searchButton").click();
+  }
 });
 
+
+
+  // Keyword Related Variables
+
+  var searchterm = document.getElementById("myInput");
+  var searchterm = searchterm.value;
+  // alert(searchterm);
+  searchRef = database.ref("search");
+
+  // Time Related Variables
+
+  timeRef = database.ref("usageTimes");
+  var date = new Date();
+  var hours = date.getHours();
+
+  // Record Time of Search
+
+  timeRef.push(hours);
+
+  // Collect Search Keyword Data
+
+  searchRef.child(searchterm).once("value", function(snapshot) {
+    if (snapshot.exists()) {
+      // alert("exists");
+      var currentValue = snapshot.child("count").val();
+      // alert("currentValue:" + currentValue);
+      var newValue = currentValue + 1;
+      // alert("NewValue:" + newValue);
+      searchRef
+        .child(searchterm)
+        .child("count")
+        .set(newValue);
+      // searchRef.child(searchterm).set(newValue);
+    } else {
+      // alert("absent");
+      var newValue = 1;
+      searchRef
+        .child(searchterm)
+        .child("name")
+        .set(searchterm);
+      searchRef
+        .child(searchterm)
+        .child("count")
+        .set(newValue);
+    }
+  });
+
+  var counter = 0 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      li[i].style.opacity = "1";
+      item = li[i] 
+      $(item).fadeIn(1000);
+      counter += 1;
+      // alert(counter)
     } else {
-      li[i].style.display = "none";
+      item = li[i] 
+      $(item).fadeOut(500); 
+      counter += 0;
+      // alert(counter)
     }
-
   }
-
-
-
+  if (counter == 0){
+    // alert(counter)
+    $("#toStaff").fadeIn(1000);
+  }
 }
 
 
+
+
+
+ // Clear Search to call back all items
 function recall() {
+
+  $("#toStaff").fadeOut(500);
+
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("myInput");
@@ -84,83 +123,82 @@ function recall() {
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
-      li[i].style.display = "";
-    }
+    li[i].style.display = "";
   }
-
+}
 
 // ========================================================
 // pagepiling animated scrolling
 // ========================================================
 $(document).ready(function() {
-	$('#pagepiling').pagepiling({
-	    menu: null,
-        direction: 'vertical',
-        verticalCentered: true,
-        sectionsColor: ['#FE3808', '#002C53', '#FE3808', '#002C53', '#002C53'],
-        anchors: ['search', 'locations', 'about', 'staff', 'contact'],
-        menu: '#mainMenu',
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            'textColor': '#000',
-            'bulletsColor': '#000',
-            'position': 'right',
-            'tooltips': ['section1', 'section2', 'section3', 'section4']
-        },
-       	normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
+  $("#pagepiling").pagepiling({
+    menu: null,
+    direction: "vertical",
+    verticalCentered: true,
+    sectionsColor: ["#FE3808", "#002C53", "#FE3808", "#002C53", "#002C53"],
+    anchors: ["search", "map", "about", "staff", "contact"],
+    menu: "#mainMenu",
+    scrollingSpeed: 700,
+    easing: "swing",
+    loopBottom: false,
+    loopTop: false,
+    css3: true,
+    navigation: {
+      textColor: "#000",
+      bulletsColor: "#000",
+      position: "right",
+      tooltips: ["section1", "section2", "section3", "section4"]
+    },
+    normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 5,
+    keyboardScrolling: true,
+    sectionSelector: ".section",
+    animateAnchor: false,
 
-		//events
-		onLeave: function(index, nextIndex, direction){},
-		afterLoad: function(anchorLink, index){},
-		afterRender: function(){},
-	});
+    //events
+    onLeave: function(index, nextIndex, direction) {},
+    afterLoad: function(anchorLink, index) {},
+    afterRender: function() {}
+  });
 });
 
 // ========================================================
 // Splash Screen Controls
 // ========================================================
 
-const colors = ['#FE3808','#002C53','#00AEE0'];
+const colors = ["#FE3808", "#002C53", "#00AEE0"];
 const numLines = 3;
 var currCount = numLines;
 const texts = document.querySelectorAll("#textClip text");
 const blobs = document.querySelectorAll("#background path");
 
 function colorBlobs() {
-    blobs.forEach(blob => {
-        blob.style.fill = colors[Math.floor(Math.random() * colors.length)];
-    });
+  blobs.forEach(blob => {
+    blob.style.fill = colors[Math.floor(Math.random() * colors.length)];
+  });
 }
 
 function nextIteration() {
-    // Change the color of all the blobs
-    colorBlobs();
+  // Change the color of all the blobs
+  colorBlobs();
 
-    // Hide the old set of lines
-    let startVal = currCount - numLines;
-    if(startVal < 0) {
-        startVal = texts.length - numLines;
-    }
-    for(let i = startVal; i < startVal + numLines; i++) {
-        texts[i].style.display = "none";
-    }
-    // Show new set of lines
-    for(let j = currCount; j < currCount + numLines; j++) {
-        texts[j].style.display = "inline";
-    }
-    currCount += numLines;
-    if(currCount >= texts.length) {
-        currCount = 0;
-    }
+  // Hide the old set of lines
+  let startVal = currCount - numLines;
+  if (startVal < 0) {
+    startVal = texts.length - numLines;
+  }
+  for (let i = startVal; i < startVal + numLines; i++) {
+    texts[i].style.display = "none";
+  }
+  // Show new set of lines
+  for (let j = currCount; j < currCount + numLines; j++) {
+    texts[j].style.display = "inline";
+  }
+  currCount += numLines;
+  if (currCount >= texts.length) {
+    currCount = 0;
+  }
 }
 
 // Since all of our blobs are using the same animation, we only
@@ -168,7 +206,6 @@ function nextIteration() {
 blobs[0].addEventListener("animationiteration", nextIteration);
 
 colorBlobs();
-
 
 // ========================================================
 // // Speech Controls
@@ -214,31 +251,36 @@ colorBlobs();
 // }
 
 // ========================================================
-   // Read From Database
+// Read From Database
 // ========================================================
 
-   $(document).ready(function() {
-    var inventoryRef = firebase.app().database().ref();
-    var itemsRef = inventoryRef.child('Items');
-  
-    itemsRef.on("child_added", snap => {
-      var item = snap.child("item_text").val();
-      var group = snap.child("group").val();
-      var short = snap.child("short_text").val();
-      var image = snap.child("image_text").val();
-      var location = snap.child("located_text").val();
-      var location_short = snap.child("located_short_text").val();
+$(document).ready(function() {
+  var inventoryRef = firebase
+    .app()
+    .database()
+    .ref();
+  var itemsRef = inventoryRef.child("Items");
 
-     $("#array").append(
-      "<li>" +
+  itemsRef.on("child_added", snap => {
+    var item = snap.child("item_text").val();
+    var group = snap.child("group").val();
+    var short = snap.child("short_text").val();
+    var image = snap.child("image_text").val();
+    var location = snap.child("located_text").val();
+    var location_short = snap.child("located_short_text").val();
+
+    $("#array").append(
+      '<li>' +
         '<div class="col-md-4">' +
         '<a href="#">' +
         item +
         '</a><div class="dbitem mb-4 box-shadow"  data-toggle="modal" data-target="#' +
         short +
         '"  >' +
-       '<div class="card-img-top" style="width:970px;">'+
-        '</div><div class="card-body '+ group +'">' +
+        '<div class="card-img-top" style="width:970px;">' +
+        '</div><div class="card-body ' +
+        group +
+        '">' +
         ' <p class="card-text"><h6>' +
         item +
         "</h6></p>" +
@@ -246,44 +288,49 @@ colorBlobs();
         "</div>" +
         "</div>" +
         "</div>" +
-        "</li>" 
-      
+        "</li>"
     );
 
     $("#modals").append(
-        '<div class="modal" id="'+ short +'" >' +
+      '<div class="modal fade" id="' +
+        short +
+        '" >' +
         ' <div class="x_button" data-dismiss="modal" aria-label="Close">&times;</div>' +
-    '<div class="row about-extra pop-row test">' +
-      '<div class="col-lg-6 modal-words">' +
-        '<h4  >'+
-          item +
-        '</h4>' +
-        '<p> Location: ' + 
-          location_short +
-        '</p>' +
-        '<p> Please ask an idea lab staff member for more help incase the item as been moved</p>' +
-      '</div>' +
-      '<div class="col-lg-6 wow fadeInUp">'+
-        ' <img src="'+image+'" class="img-fluid item-images" alt="" />'+
-      '</div>'+
-    '</div></div>'
+        '<div class="modal-dialog"><div class="modal-content">' +
+        '<div class="row about-extra pop-row test">' +
+        '<div class="col-lg-6 modal-words">' +
+        "<h4  >" +
+        item +
+        "</h4>" +
+        "<p> Location: " +
+        location_short +
+        "</p>" +
+        "<p> Please ask an idea lab staff member for more help incase the item as been moved</p>" +
+        "</div>" +
+        '<div class="col-lg-6 wow fadeInUp">' +
+        ' <img src="' +
+        image +
+        '" class="img-fluid item-images" alt="" />' +
+        "</div>" +
+        "</div>"+
+        '</div></div>'+
+        "</div>"
     );
   });
 });
 
-
-$('.close').modal('toggle');
+$(".close").modal("toggle");
 
 // ========================================================
 // NavBar
 // ========================================================
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
 // ========================================================
 // Contact Bar
@@ -297,9 +344,8 @@ function closeContact() {
   document.getElementById("contactNav").style.width = "0%";
 }
 
-
-// ========================================================  
-// Write Reuqests To Database 
+// ========================================================
+// Write Reuqests To Database
 // ========================================================
 var first_name = document.getElementById("first_name");
 var email_address = document.getElementById("email_address");
@@ -315,18 +361,23 @@ function contactSubmit() {
   var email = email_address.value;
   var comment = comments.value;
 
-  var ref = firebase.app().database().ref();
-      var requestRef = ref.child('requests').push();
+  var ref = firebase
+    .app()
+    .database()
+    .ref();
+  var requestRef = ref.child("requests").push();
 
-      window.alert("Thank you for sending your request! we will get back to you soon!");
+  window.alert(
+    "Thank you for sending your request! we will get back to you soon!"
+  );
 
-      requestRef.child("name").set(name);
-      requestRef.child("email").set(email);
-      requestRef.child("comment").set(comment);
-      // firebaseRef.push().set(messageText);
+  requestRef.child("name").set(name);
+  requestRef.child("email").set(email);
+  requestRef.child("comment").set(comment);
+  // firebaseRef.push().set(messageText);
 }
 
-// ========================================================  
+// ========================================================
 // Google Script for Request Data via email
 // ========================================================
 
@@ -335,26 +386,29 @@ function contactSubmit() {
   function getFormData(form) {
     var elements = form.elements;
     var honeypot;
-    var fields = Object.keys(elements).filter(function(k) {
-      if (elements[k].name === "honeypot") {
-        honeypot = elements[k].value;
-        return false;
-      }
-      return true;
-    }).map(function(k) {
-      if(elements[k].name !== undefined) {
-        return elements[k].name;
-      // special case for Edge's html collection
-      }else if(elements[k].length > 0){
-        return elements[k].item(0).name;
-      }
-    }).filter(function(item, pos, self) {
-      return self.indexOf(item) == pos && item;
-    });
+    var fields = Object.keys(elements)
+      .filter(function(k) {
+        if (elements[k].name === "honeypot") {
+          honeypot = elements[k].value;
+          return false;
+        }
+        return true;
+      })
+      .map(function(k) {
+        if (elements[k].name !== undefined) {
+          return elements[k].name;
+          // special case for Edge's html collection
+        } else if (elements[k].length > 0) {
+          return elements[k].item(0).name;
+        }
+      })
+      .filter(function(item, pos, self) {
+        return self.indexOf(item) == pos && item;
+      });
     var formData = {};
-    fields.forEach(function(name){
+    fields.forEach(function(name) {
       var element = elements[name];
-      
+
       // singular form elements just have one value
       formData[name] = element.value;
       // when our element has multiple items, get their values
@@ -366,18 +420,18 @@ function contactSubmit() {
             data.push(item.value);
           }
         }
-        formData[name] = data.join(', ');
+        formData[name] = data.join(", ");
       }
     });
     // add form-specific values into the data
     formData.formDataNameOrder = JSON.stringify(fields);
     formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-    formData.formGoogleSend
-      = form.dataset.email || ""; // no email by default
-    return {data: formData, honeypot: honeypot};
+    formData.formGoogleSend = form.dataset.email || ""; // no email by default
+    return { data: formData, honeypot: honeypot };
   }
-  function handleFormSubmit(event) {  // handles form submit without any jquery
-    event.preventDefault();           // we are submitting via xhr below
+  function handleFormSubmit(event) {
+    // handles form submit without any jquery
+    event.preventDefault(); // we are submitting via xhr below
     var form = event.target;
     var formData = getFormData(form);
     var data = formData.data;
@@ -388,36 +442,38 @@ function contactSubmit() {
     disableAllButtons(form);
     var url = form.action;
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
+    xhr.open("POST", url);
     // xhr.withCredentials = true;
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          form.reset();
-          var formElements = form.querySelector(".form-elements")
-          if (formElements) {
-            formElements.style.display = "none"; // hide form
-          }
-          var thankYouMessage = form.querySelector(".thankyou_message");
-          if (thankYouMessage) {
-            thankYouMessage.style.display = "block";
-          }
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        form.reset();
+        var formElements = form.querySelector(".form-elements");
+        if (formElements) {
+          formElements.style.display = "none"; // hide form
         }
+        var thankYouMessage = form.querySelector(".thankyou_message");
+        if (thankYouMessage) {
+          thankYouMessage.style.display = "block";
+        }
+      }
     };
     // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
+    var encoded = Object.keys(data)
+      .map(function(k) {
         return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
-    }).join('&');
+      })
+      .join("&");
     xhr.send(encoded);
   }
-  
+
   function loaded() {
     // bind to the submit event of our form
     var forms = document.querySelectorAll("form.gform");
     for (var i = 0; i < forms.length; i++) {
       forms[i].addEventListener("submit", handleFormSubmit, false);
     }
-  };
+  }
   document.addEventListener("DOMContentLoaded", loaded, false);
   function disableAllButtons(form) {
     var buttons = form.querySelectorAll("button");
